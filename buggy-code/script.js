46347a0b7a7ccbabe02fd9d53643c3dd7ac9f789
Harshith-Daraboina <23bcs037@iiitdwd.ac.in>
2025-10-25 17:06:45 +0530

    buggyone

--- buggy-code/script.js
+++ buggy-code/script.js
@@ -2,30 +2,30 @@
 
 function sum(a, b) {
   // Bug 1: Should return a + b, but it's currently subtracting
-  return a + b;
+  return a - b;
 }
 
 function multiply(a, b) {
   // Bug 2: Should return a * b, but it's returning the wrong value
-  return a*b;
+  return a;
 }
 
 function isEven(num) {
   // Bug 3: Should return true for even numbers, false for odd
   // Currently it returns false for everything
-  return num % 2 === 0;
+  return false;
 }
 
 function findMax(numbers) {
   // Bug 4: Should return the maximum number in the array
   // Currently it returns the first element instead
-  return Math.max(...numbers);
+  return numbers[0];
 }
 
 function reverseString(str) {
   // Bug 5: Should return the reversed string
   // Currently it just returns the original string
-  return str.split('').reverse().join('');
+  return str;
 }
 
 module.exports = {
